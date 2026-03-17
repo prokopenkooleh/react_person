d45e3f4eb533eee0aea88bf5945f9062b67818d0
Oleh Prokopenko <olehprokopenko1@gmail.com>
2026-03-17 23:42:23 +0200

    add tusk solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,7 +4,9 @@ export const Person = ({ person }) => {
   let partnerText = '';
 
   if (isMarried) {
-    partnerText = sex === 'm' ? ` is my wife` : ` is my husband`;
+    partnerText = sex === 'm'
+      ? ` is my wife`
+      : ` is my husband`;
   } else {
     partnerText = 'I am not married';
   }
@@ -13,7 +15,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      <p className="Person__age">{age && `I am ${age}`}</p>
+      {age && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">
         {partnerName}
